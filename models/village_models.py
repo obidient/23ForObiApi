e443fc531ea2e845a7fcfe63d32c5836ab0e113e
Oluwafemi Adenuga <phemmylintry@gmail.com>
2022-07-21 10:17:29 +0100

    .

--- models/village_models.py
+++ models/village_models.py
@@ -2,7 +2,6 @@ import datetime
 from uuid import uuid4
 
 import bigfastapi.db.database as db
-from bigfastapi.models.location_models import Location
 from bigfastapi.models.user_models import User
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
